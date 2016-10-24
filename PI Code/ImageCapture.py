1ab3f2174ef7ba80ea7011df54f27daabc58fbfe
Connor Anderson <connora@stanford.edu>
2016-10-24 11:31:12 -0700

    Please enter the commit message for your changes. Lines starting

--- PI Code/ImageCapture.py
+++ PI Code/ImageCapture.py
@@ -10,81 +10,99 @@ from picamera import PiCamera
 GPIO_TRIGGER = 23
 GPIO_ECHO    = 24
 GPIO_FLASH   = 25
+GPIO_CAPTURE = 26
 
 CAMERA_CAPTURE_DELAY = 0.025 # Units S
 FLASH_ON_DELAY = 0.001
 CAMERA_WARM_TIME = 2;
 
+# ----------- Initialization -----------------------
 
-#------------ DISTANCE CAPTURE ROUTINE -----------------------
-
-# Use BCM GPIO references
-# instead of physical pin numbers
-GPIO.setmode(GPIO.BCM)
-
-print "Ultrasonic Measurement"
-
-# Set pins as output and input
-GPIO.setup(GPIO_TRIGGER,GPIO.OUT)  # Trigger
-GPIO.setup(GPIO_ECHO,GPIO.IN)      # Echo
-
-# Set trigger to False (Low)
-GPIO.output(GPIO_TRIGGER, False)
-
-# Allow module to settle
-time.sleep(0.5)
-
-# Send 10us pulse to trigger
-GPIO.output(GPIO_TRIGGER, True)
-time.sleep(0.00001)
-GPIO.output(GPIO_TRIGGER, False)
-start = time.time()
-
-while GPIO.input(GPIO_ECHO)==0:
-  start = time.time()
-
-while GPIO.input(GPIO_ECHO)==1:
-  stop = time.time()
-
-# Calculate pulse length
-elapsed = stop-start
-
-# Distance pulse travelled in that time is time
-# multiplied by the speed of sound (cm/s)
-distance = elapsed * 34300
-
-# That was the distance there and back so halve the value
-distance = distance / 2
-
-print "Distance : %.1f" % distance
-
-# Reset GPIO settings
-GPIO.cleanup()
-
-#------------ IMAGE CAPTURE ROUTINE -----------------------
-
+# Initialize camera
 camera = PiCamera()
 camera.resolution = (1024, 768)
-#camera.start_preview()
-# Camera warm-up time
-sleep(CAMERA_WARM_TIME)
-camera.capture('{timestamp:%H%M%S}-{counter:03d}.jpg') # Individually managed filename
-# Give camera time to capture
-time.sleep(CAMERA_CAPTURE_DELAY)
 
-#------------ FLASH ON -----------------------
-GPIO.output(GPIO_FLASH, True)
-# Give flash time to come on
-time.sleep(FLASH_ON_DELAY)
 
-#------------ IMAGE CAPTURE ROUTINE -----------------------
-camera.capture('{timestamp:%H%M%S}-{counter:03d}.jpg') # Individually managed filename
-# Give camera time to capture
-time.sleep(CAMERA_CAPTURE_DELAY)
+# TODO wait until button is pressed
+while true:
 
-#------------ FLASH OFF -----------------------
-GPIO.output(GPIO_FLASH, False)
-# Give flash time to come on
-time.sleep(FLASH_ON_DELAY)
+	if GPIO.input(GPIO_CAPTURE)==1: # Ready to take picture
+
+		# loop the following and then wait again
+
+
+		#------------ DISTANCE CAPTURE ROUTINE -----------------------
+
+		# Use BCM GPIO references
+		# instead of physical pin numbers
+		GPIO.setmode(GPIO.BCM)
+
+		print "Ultrasonic Measurement"
+
+		# Set pins as output and input
+		GPIO.setup(GPIO_TRIGGER,GPIO.OUT)  # Trigger
+		GPIO.setup(GPIO_ECHO,GPIO.IN)      # Echo
+
+		# Set trigger to False (Low)
+		GPIO.output(GPIO_TRIGGER, False)
+
+		# Allow module to settle
+		time.sleep(0.5)
+
+		# Send 10us pulse to trigger
+		GPIO.output(GPIO_TRIGGER, True)
+		time.sleep(0.00001)
+		GPIO.output(GPIO_TRIGGER, False)
+		start = time.time()
+
+		while GPIO.input(GPIO_ECHO)==0:
+		  start = time.time()
+
+		while GPIO.input(GPIO_ECHO)==1:
+		  stop = time.time()
+
+		# Calculate pulse length
+		elapsed = stop-start
+
+		# Distance pulse travelled in that time is time
+		# multiplied by the speed of sound (cm/s)
+		distance = elapsed * 34300
+
+		# That was the distance there and back so halve the value
+		distance = distance / 2
+
+		print "Distance : %.1f" % distance
+
+		# Reset GPIO settings
+		GPIO.cleanup()
+
+		#------------ IMAGE CAPTURE ROUTINE -----------------------
+
+		camera.start_preview()
+		# Camera warm-up time
+		sleep(CAMERA_WARM_TIME)
+		camera.capture('{timestamp:%H%M%S}-{counter:03d}.jpg') # Individually managed filename
+		# Give camera time to capture
+		time.sleep(CAMERA_CAPTURE_DELAY)
+
+		#------------ FLASH ON -----------------------
+		GPIO.output(GPIO_FLASH, True)
+		# Give flash time to come on
+		time.sleep(FLASH_ON_DELAY)
+
+		#------------ IMAGE CAPTURE ROUTINE -----------------------
+		camera.capture('{timestamp:%H%M%S}-{counter:03d}.jpg') # Individually managed filename
+		# Give camera time to capture
+		time.sleep(CAMERA_CAPTURE_DELAY)
+
+		#------------ FLASH OFF -----------------------
+		GPIO.output(GPIO_FLASH, False)
+		# Give flash time to turn off
+		time.sleep(FLASH_ON_DELAY)
+	else:
+
+		time.sleep(0.5)
+
+		
 
 
